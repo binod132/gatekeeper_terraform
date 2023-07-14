8f8708e4aad7ed38010f8fa17bfa821dea8aca00
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-07-14 15:26:09 +0545

    updating kubeconfig 1

--- k8s-gatekeeper/main.tf
+++ k8s-gatekeeper/main.tf
@@ -4,11 +4,13 @@
 #}
 provider "kubernetes" {
   config_path = "${path.root}/.kube/config"
+  config_context = "minikube"
 }
 
 provider "helm" {
   kubernetes {
   config_path = "${path.root}/.kube/config"
+  config_context = "minikube"
   }
 }
 
