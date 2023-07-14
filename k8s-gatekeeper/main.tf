6ead18a10e96d1023805df9efa49401033ddaac9
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-07-14 15:14:40 +0545

    updated files on main.tf

--- k8s-gatekeeper/main.tf
+++ k8s-gatekeeper/main.tf
@@ -1,7 +1,7 @@
-provider "aws" {
-  version = "~> 4.0"
-  region  = "us-west-1"
-}
+#provider "aws" {
+#  version = "~> 4.0"
+#  region  = "us-west-1"
+#}
 provider "kubernetes" {
   config_path = "${path.root}/.kube/config"
 }
@@ -12,22 +12,22 @@ provider "helm" {
   }
 }
 
-resource "kubernetes_namespace" "gatekeeper" {
-  metadata {
-   name = var.namespace
- }
-}
+#resource "kubernetes_namespace" "gatekeeper" {
+#  metadata {
+#   name = var.namespace
+# }
+#}
 
 # Add the Helm repository
-resource "helm_release" "gatekeeper" {
-  name = "gatekeeper"
-  repository  = "https://open-policy-agent.github.io/gatekeeper/charts"
-  chart      = "gatekeeper"
-  namespace  = "gatekeeper-system"
-  depends_on = [
-    kubernetes_namespace.gatekeeper
-  ]
-}
+#resource "helm_release" "gatekeeper" {
+#  name = "gatekeeper"
+#  repository  = "https://open-policy-agent.github.io/gatekeeper/charts"
+#  chart      = "gatekeeper"
+#  namespace  = "gatekeeper-system"
+#  depends_on = [
+#    kubernetes_namespace.gatekeeper
+#  ]
+#}
 
 resource "helm_release" "gatekeeper-templates" {
   chart     = "${path.root}/helm-gatekeeper-templates"
@@ -35,9 +35,9 @@ resource "helm_release" "gatekeeper-templates" {
   namespace = var.namespace
   version   = "0.0.3"
 
-  depends_on = [
-    helm_release.gatekeeper
-  ]
+#  depends_on = [
+#    helm_release.gatekeeper
+#  ]
 }
 
 resource "helm_release" "gatekeeper-constraints" {
@@ -46,7 +46,7 @@ resource "helm_release" "gatekeeper-constraints" {
   namespace = var.namespace
   version   = "0.0.3"
 
-  depends_on = [
-    helm_release.gatekeeper-templates
-  ]
+#  depends_on = [
+#    helm_release.gatekeeper-templates
+#  ]
 }
