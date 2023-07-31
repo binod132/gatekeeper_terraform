34baab45867d52070bfa0a10455768bca8ce59a6
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-07-31 21:58:59 +0545

    adding namespace module

--- modules/resources/main.tf
+++ modules/resources/main.tf
@@ -1,7 +1,6 @@
 resource "helm_release" "namespace_template" {
   chart     = ".//modules/resources/namespace"
   name      = "namespace_template"
-  type =  infrastructure
 #   namespace = var.namespace
 #   version   = "0.0.3"
 
