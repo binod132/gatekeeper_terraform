ce4be99c4bb9abd76578f7eb4f1c4c4dd25ff533
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-08-25 03:22:17 +0545

    helm varible undate

--- modules/namespace/main.tf
+++ modules/namespace/main.tf
@@ -2,6 +2,6 @@ resource "helm_release" "namespace-template" {
   chart     = ".//modules/namespace/helm-namespace"
   name      = "namespace-template"
   values = [
-    templatefile(".//modules/namespace/helm-namespace/templates/namespace.yaml", {name_namespace = "${var.name_namespace}"}, lable_namespace="${var.lable_namespace}")
+    templatefile(".//modules/namespace/helm-namespace/templates/namespace.yaml", {name_namespace = "${var.name_namespace}"}, {lable_namespace="${var.lable_namespace}"})
   ]
 }
