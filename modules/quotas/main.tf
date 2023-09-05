9f2bfcb6e3710a70202a3f58e4cc10a0cbed588d
Binod Adhikari <074msice006.binod@pcampus.edu.np>
2023-09-05 22:14:26 +0545

    adding clusterrole module

--- modules/quotas/main.tf
+++ modules/quotas/main.tf
@@ -1,4 +1,13 @@
 resource "helm_release" "namespace-template" {
   chart     = ".//modules/quotas/helm-quota"
   name      = "quota-template"
+  set {
+    name  = "name_namespace"  # A key in the Helm chart's values.yaml
+    value = var.name_namespace
+    }
+
+  set {
+    name  = "label_namespace"  # A key in the Helm chart's values.yaml
+    value = var.label_namespace
+    } 
 }
